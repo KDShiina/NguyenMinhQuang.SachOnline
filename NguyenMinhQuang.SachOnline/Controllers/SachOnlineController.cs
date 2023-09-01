64defe2b84a9494376ea1256f53b8e8be21e01bc
shiin <shiin@MINHQUANG>
2023-09-01 17:17:21 +0700

    Add project files.

---
+++ NguyenMinhQuang.SachOnline/Controllers/SachOnlineController.cs
@@ -0,0 +1,31 @@
+﻿using NguyenMinhQuang.SachOnline.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BaiTap.Controllers
+{
+    public class SachOnlineController : Controller
+    {
+        DataClasses1DataContext db = new DataClasses1DataContext(@"Data Source=MINHQUANG;Initial Catalog=SachOnline;Integrated Security=True");
+
+        // GET: SachOnline
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Search(string keyword)
+        {
+           var searchResults = db.SACHes.Where(book => book.TenSach.Contains(keyword) || book.MoTa.Contains(keyword)).ToList();
+
+            return View(searchResults);
+        }
+
+
+
+
+    }
+}
---
+++ NguyenMinhQuang.SachOnline/Controllers/TrangChuController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Common;
+using System.Data.Linq;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NguyenMinhQuang.SachOnline.Models;
+namespace BaiTap.Controllers
+{
+    public class TrangChuController : Controller
+    {
+        DataClasses1DataContext db = new DataClasses1DataContext(@"Data Source=MINHQUANG;Initial Catalog=SachOnline;Integrated Security=True");
+        // GET: TrangChu
+        public ActionResult SachOnline()
+        {
+            return View();
+        }
+
+        public ActionResult ChuDePartial()
+        {
+            var cd = from c in db.CHUDEs select c;
+            return PartialView(cd);
+        }
+        public ActionResult NBXPartial()
+        {
+            var cd = from c in db.NHAXUATBANs select c;
+
+            return PartialView(cd);
+        }
+        public ActionResult NavPartial()
+        {
+            return PartialView();
+        }
+        public ActionResult FooterPartial()
+        {
+            return PartialView();
+        }
+        public ActionResult SliderPartial()
+        {
+            return PartialView();
+        }
+        public ActionResult SachBanNhieuPartial()
+        {
+            return PartialView();
+        }
+
+
+    }
+
+
+}
